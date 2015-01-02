c6c5e07a08225fe2889e5576844b78e288f7f128
Akshay S Dinesh <asdofindia@gmail.com>
2015-01-03 03:23:40 +0530

    grambot now can send thousands simultaneosuly

--- src/feed/grambot.sh
+++ src/feed/grambot.sh
@@ -6,14 +6,20 @@
 # You need to edit two things here: the TGDIR and the -p argument value
 # Refer README.md for more
 
-# The directory in which tg-cli is stored is $3
+# The directory in which tg-cli is stored is $1
 
 RANDFILE=$RANDOM
 echo $2 > $RANDFILE
 
+COMMAND=""
+for CHAT in "${@:3}"
+do
+  COMMAND="${COMMAND}send_text ${CHAT} ${RANDFILE}\n"
+done
+
 # Remove the "-p kl" if you don't want a separate profile for the bot.
 # Otherwise, create a profile named kl, or change kl to your profile name
 # The profile need to be saved in config.sample in tg-cli directory
-echo "send_text $1 $RANDFILE" | $3/bin/telegram-cli -W -D -k $3/server.pub -c $3/config.sample
+#echo "${COMMAND}" | $1/bin/telegram-cli -W -D -k $1/server.pub -c $1/config.sample
 
 rm -f $RANDFILE

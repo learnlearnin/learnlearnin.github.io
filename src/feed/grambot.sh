9256e5e7f9a2d2d8cf80cf3065ec87d3d7df86ae
Akshay S Dinesh <asdofindia@gmail.com>
2015-01-03 02:50:59 +0530

    push options

---
+++ src/feed/grambot.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+# This file is GPL v3.
+
+# This script does the task of invoking tg-cli with the right arguments
+# You need to edit two things here: the TGDIR and the -p argument value
+# Refer README.md for more
+
+# The directory in which tg-cli is stored is $3
+
+RANDFILE=$RANDOM
+echo $2 > $RANDFILE
+
+# Remove the "-p kl" if you don't want a separate profile for the bot.
+# Otherwise, create a profile named kl, or change kl to your profile name
+# The profile need to be saved in config.sample in tg-cli directory
+echo "send_text $1 $RANDFILE" | $3/bin/telegram-cli -W -D -k $3/server.pub -c $3/config.sample
+
+rm -f $RANDFILE

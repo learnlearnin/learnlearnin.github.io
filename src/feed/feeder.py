0001bcf0a30b1d38290e9b42e06e3898eaaf35b4
Akshay S Dinesh <asdofindia@gmail.com>
2015-01-03 00:35:41 +0530

    major improvements in feeder

--- src/feed/feeder.py
+++ src/feed/feeder.py
@@ -5,93 +5,31 @@ import os, sys
 import json
 from pprint import pprint
 from xml.dom.minidom import parseString
-import time
-import uuid
 
-def gettime():
-    return time.strftime("%Y-%m-%dT%H:%M:%SZ",time.gmtime())
+import tools
 
-def getuuid():
-    urn=uuid.uuid4()
-    return str(urn)
-
-def entryprepare(entry):
-    if not 'title' in entry:
-        raise Warning("entry with no title")
-    if not 'content' in entry:
-        raise Warning(entry['title']+" has no content")
-    entry['link'] = entry['link'] if 'link' in entry else "http://learnlearn.in/"
-    if entry['link'].startswith("/"):
-        entry['link']="http://learnlearn.in"+entry['link']
-        entry['content']+="""
-
-Read more at """+entry['link']
-    entry['published'] = entry['published'] if 'published' in entry else gettime()
-    if "dirty" in entry:
-        if not entry['dirty']=="false":
-            entry['updated'] = gettime()
-            entry['dirty']="false"
-    entry['id'] = entry['id']  if 'id' in entry else "urn:uuid:"+getuuid()
-    entry['contenttype'] = entry['contenttype'] if  "contenttype" in entry else "text"
-    entry['author'] = entry['author'] if "author" in entry else "Akshay S Dinesh"
-    entry['authoremail'] = entry['authoremail'] if  "authoremail" in entry else "akshay@autistici.org"
-    entry['authoruri'] = entry['authoruri'] if "authoruri" in entry else "http://learnlearn.in/about/"
-    return entry
+import argparse
 
 
-def entrytoxml(entry):
-    entrystring="<entry>"
-    entrystring+="<title>"+entry['title']+"</title>"
-    entrystring+='<link href="'+entry['link']+'" />'
-    entrystring+='<id>'+entry['id']+'</id>'
-    if hasattr(entry, "updated"):
-        entrystring+='<updated>'+entry['updated']+'</updated>'
-    entrystring+='<published>'+entry['published']+'</published>'
-    entrystring+='<author><name>'+entry['author']+'</name><uri>'+entry['authoruri']+'</uri><email>'+entry['authoremail']+'</email></author>'
-    entrystring+='<content type="'+entry['contenttype']+'">'+entry['content']+'</content>'
-    entrystring+='</entry>'
-    return entrystring
+parser = argparse.ArgumentParser(description="A command line multi feed updater")
+parser.add_argument('-f','--file', default="feed.json")
+# parser.add_argument('-a','--add', nargs='*', help="adds an entry to the feeds with the values following used as title, content, and link respectively", metavar=('content'))
+subparsers = parser.add_subparsers()
 
-def main(*args):
-    try:
-        filename = args[0]
-    except IndexError:
-        filename = "feed.json"
+parser_add = subparsers.add_parser('add', help="adds an entry to feed")
+parser_add.add_argument('content')
+parser_add.add_argument('-t','--title')
+parser_add.add_argument('-l','--link')
+parser_add.set_defaults(func=tools.addentry)
 
-    dirname = os.path.dirname(os.path.realpath(sys.argv[0]))
-    filepath = os.path.join(dirname,filename)
-    atompath = os.path.join(os.path.dirname(dirname),"files",filename.split('.')[0]+".atom")
+parser_sync = subparsers.add_parser('sync', help="syncs the feed to the json")
+parser_sync.set_defaults(func=tools.sync)
 
-    if not os.path.isfile(filepath):
-        print(filepath,"does not exist")
-        return
-    with open(filepath, 'r') as jsonfile:
-        print("loading",filepath)
-        feeds = json.load(jsonfile)
-        pprint(feeds)
-        feedstring="""<?xml version="1.0" encoding="utf-8"?><feed xmlns="http://www.w3.org/2005/Atom"><title type="text">Learn Learnin'</title><subtitle type="html">The quintessential insight aggregator</subtitle>"""
-        feedstring+="<updated>"+gettime()+"</updated>"
-        feedstring+="<id>http://learnlearn.in/feed.atom</id>"
-        feedstring+='<link rel="alternate" type="text/html" hreflang="en" href="http://learnlearn.in/"/>'
-        feedstring+='<link rel="self" type="application/atom+xml" href="http://learnlearn.in/feed.atom"/>'
-        feedstring+='<generator uri="http://github.com/learnlearnin/learnlearnin.github.io/blob/source/src/feed/feeder.py" version="1.0">Hand written python json to feed converter</generator>'
-        preparedentries=[]
-        for entry in feeds['entries']:
-            preparedentries.append(entryprepare(entry))
-        feeds['entries']=preparedentries
-        for entry in feeds['entries']:
-            feedstring+=entrytoxml(entry)
-        feedstring+="</feed>"
+parser_deletelast = subparsers.add_parser('deletelast', help="deletes the last feed from json and feed")
+parser_deletelast.set_defaults(func=tools.deletelast)
 
-    with open(filepath, 'w') as jsonfile:
-        json.dump(feeds,jsonfile,indent=1)
+parser_push = subparsers.add_parser('push', help="pushes last update to social networks")
+parser_push.set_defaults(func=tools.push)
 
-    atomxml = parseString(feedstring)
-    with open(atompath, 'w') as atomfile:
-        atomfile.write(atomxml.toprettyxml())
-
-
-
-if __name__=="__main__":
-    print("Parsing json")
-    main()
+args = parser.parse_args()
+args.func(args)

ec22325705ca2e0542f0ff6554ebae1fb8444490
Akshay S Dinesh <asdofindia@gmail.com>
2015-04-07 21:25:37 +0530

    moving song focus out

--- src/files/projects/songfocus/songfocus.js
+++
@@ -1,7 +0,0 @@
-all=document.querySelectorAll("article img")
-for (var i=0; i< all.length; i++){
-  all[i].setAttribute("data-src", all[i].getAttribute("src"))
-  all[i].src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==";
-  all[i].setAttribute("onload","lzld(this)")
-}
-

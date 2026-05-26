d388d8882a8b00c421815369e499a9d787e8f138
Akshay S Dinesh <akshay@learnlearn.in>
2026-05-26 16:51:25 +0530

    feat: support wifi in QR

--- content/tools/qr.md
+++ content/tools/qr.md
@@ -5,7 +5,25 @@ js = ['qr']
 aliases = ['/qr/']
 +++
 
+<fieldset>
+  <legend>Select the kind of QR code:</legend>
+
+  <div>
+    <input type="radio" id="regular" name="type" value="regular" checked />
+    <label for="regular">Regular</label>
+  </div>
+
+  <div>
+    <input type="radio" id="wifi" name="type" value="wifi" />
+    <label for="wifi">WiFi</label>
+  </div>
+</fieldset>
+
 <textarea id="qr-input" type="text" style="width: 100%"></textarea>
+<div id="wifi-section">
+<label for="wifi-ap">Access Point: </label><input id="wifi-ap" type="text">
+<label for="wifi-pass">Password: </label><input id="wifi-pass" type="text">
+</div>
 
 <canvas id="qr-canvas"></canvas>
 
--- static/js/qr.js
+++ static/js/qr.js
@@ -1,13 +1,52 @@
 import QRCode from "https://esm.sh/qrcode@1.5.1";
+
 const canvas = document.getElementById("qr-canvas");
 const text = document.getElementById("qr-input");
 const image = document.getElementById("qr-image");
-canvas.style.display = "none";
-image.style.display = "none";
+const wifiAp = document.getElementById("wifi-ap");
+const wifiPass = document.getElementById("wifi-pass");
+const regularSwitch = document.getElementById("regular");
+const wifiSwitch = document.getElementById("wifi");
+const wifiSection = document.getElementById("wifi-section");
+
+const hide = (...nodes) => {
+  nodes.forEach((n) => (n.style.display = "none"));
+};
+
+const show = (...nodes) => {
+  nodes.forEach((n) => (n.style.display = "revert"));
+};
+
+const getMode = () => {
+  if (regularSwitch.checked) return "regular";
+  if (wifiSwitch.checked) return "wifi";
+};
+
+let mode = getMode();
+
+const repaint = () => {
+  mode = getMode();
+  switch (mode) {
+    case "regular":
+      hide(wifiSection);
+      show(text);
+      break;
+    case "wifi":
+      hide(text);
+      show(wifiSection);
+      break;
+  }
+};
+
+hide(canvas);
+repaint();
+
 const DEFAULT = "https://learnlearn.in/tools/qr/";
 text.defaultValue = DEFAULT;
 text.value = DEFAULT;
+
 const parent = document.querySelector("article");
+
 function getContentWidth(element) {
   let widthWithPaddings = element.clientWidth;
   const elementComputedStyle = window.getComputedStyle(element, null);
@@ -17,6 +56,7 @@ function getContentWidth(element) {
     parseFloat(elementComputedStyle.paddingRight)
   );
 }
+
 function write(text) {
   const ctx = canvas.getContext("2d");
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -26,8 +66,9 @@ function write(text) {
   ctx.font = "48px serif";
   ctx.fillText(text, 10, 50);
 }
+
 const create = (text) => {
-  image.style.display = "none";
+  hide(image);
   const width = getContentWidth(parent);
   QRCode.toCanvas(
     canvas,
@@ -41,16 +82,52 @@ const create = (text) => {
         write(error);
       }
       image.src = canvas.toDataURL("image/png");
-      image.style.display = "block";
+      show(image);
     },
   );
 };
 
-create(DEFAULT);
-text.addEventListener("input", () => create(text.value));
-text.addEventListener("change", () => create(text.value));
+const escape = (
+  string,
+  charsToEscape = ["\\", ";", ",", '"', ":"],
+  escapeChar = "\\",
+) => {
+  let escaped = "";
+  for (let i = 0; i < string.length; i++) {
+    const char = string[i];
+    if (charsToEscape.includes(char)) {
+      escaped = escaped + "\\" + char;
+    } else {
+      escaped = escaped + char;
+    }
+  }
+  return escaped;
+};
+
+const update = () => {
+  switch (mode) {
+    case "regular":
+      create(text.value);
+      break;
+    case "wifi":
+      const ssid = escape(wifiAp.value);
+      const pass = escape(wifiPass.value);
+      create(`WIFI:T:WPA;S:${ssid};P:${pass};;`);
+      break;
+  }
+};
+
+update();
+
+text.addEventListener("input", update);
+text.addEventListener("change", update);
+wifiAp.addEventListener("input", update);
+wifiAp.addEventListener("change", update);
+wifiPass.addEventListener("input", update);
+wifiPass.addEventListener("change", update);
+regular.addEventListener("change", repaint);
+wifi.addEventListener("change", repaint);
 const resizeObserver = new ResizeObserver(() => {
-  create(text.value);
+  update();
 });
 resizeObserver.observe(parent);
-download.addEventList;
